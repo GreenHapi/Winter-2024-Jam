95365008f3684f4e53fe12ed2120cf6880599680
RockAndForest <20582420-RockAndForest@users.noreply.gitlab.com>
2024-12-22 16:22:33 +0200

    Add StartScreen and Main menu with New Game and Quit

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Managers {
+public class GameManager : MonoBehaviour {
+    [SerializeField] private int _newGameSceneIndex = 1;
+
+    public void NewGame() {
+        SceneManager.LoadScene(_newGameSceneIndex);
+    }
+
+    public static void Quit() {
+        #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
+    }
+}
+}
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UI {
+public class MainMenu : MonoBehaviour {
+    public void Show() {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide() {
+        gameObject.SetActive(false);
+    }
+}
+}
