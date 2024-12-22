243c0e8ec306a8709fb16aff0b7eda144eb0c9e4
RockAndForest <20582420-RockAndForest@users.noreply.gitlab.com>
2024-12-22 18:49:29 +0200

    Add House prefab with Melt & Freeze

--- Assets/Scripts/Units/Buildings/House.cs
+++ Assets/Scripts/Units/Buildings/House.cs
@@ -1,12 +1,24 @@
-﻿using WinterJam.Units.Characters;
+﻿using NaughtyAttributes;
+using UnityEngine;
+using WinterJam.Units.Characters;
 
-namespace WinterJam.Units.Buildings
-{
-    public class House : Unit, IInteractable
-    {
-        public void TryInteract(Character character)
-        {
-            
-        }
+namespace WinterJam.Units.Buildings {
+public class House : Unit, IInteractable {
+    [SerializeField] private GameObject _frozen;
+    [SerializeField] private GameObject _melted;
+
+    public void TryInteract(Character character) { }
+
+    [Button]
+    private void Melt() {
+        _frozen.SetActive(false);
+        _melted.SetActive(true);
     }
+
+    [Button]
+    private void Freeze() {
+        _frozen.SetActive(true);
+        _melted.SetActive(false);
+    }
+}
 }
