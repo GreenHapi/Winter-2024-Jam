2867cfcd581cc8e6c30371e793fca5fca3734e22
RockAndForest <20582420-RockAndForest@users.noreply.gitlab.com>
2024-12-22 18:18:26 +0200

    Add Frozen Cabin prefab

---
+++ Assets/Scripts/Float.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class FloatUpDown : MonoBehaviour {
+    [SerializeField]
+    [Header("Floating Parameters")]
+    [Tooltip("Maximum offset from the starting position in the Y-axis.")]
+    private float _maxOffset = 1f;
+
+    [SerializeField] [Tooltip("Speed of the floating motion.")]
+    private float _speed = 2f;
+
+    private Vector3 _startPosition;
+
+    void Start() {
+        _startPosition = transform.position;
+    }
+
+    void Update() {
+        float newY = _startPosition.y + Mathf.Sin(Time.time * _speed) * _maxOffset;
+
+        transform.position = new Vector3(_startPosition.x, newY, _startPosition.z);
+    }
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    [SerializeField] private float _speed = 100f;
+
+    private void Update() {
+        transform.RotateAround(transform.position, Vector3.up, _speed * Time.deltaTime);
+    }
+}
