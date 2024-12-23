4dc9b519ac077fc9114e2e883578a867fac91c8d
RockAndForest <20582420-RockAndForest@users.noreply.gitlab.com>
2024-12-24 00:35:57 +0200

    Merge of "rock-and-forest" and "origin/main"

---
+++ Assets/Scripts/UI/PauseUI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class PauseUI : MonoBehaviour
+    {
+        [SerializeField] private UIScreen _pauseUI;
+        private bool _paused = false;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SwitchPauseUI();
+            }
+        }
+
+        private void SwitchPauseUI()
+        {
+            if (!_paused)
+            {
+                _pauseUI.Show();
+                _paused = false;
+            }
+            else
+            {
+                _pauseUI.Hide();
+                _paused = true;
+            }
+        }
+    }
+}
